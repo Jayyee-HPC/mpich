1f90d4698ca77e339ab613221dba4614721df9d7
Jayyee-HPC <jie.yang@marquette.edu>
2021-03-17 17:49:05 -0500

    coll: add check approaches for scatterv, allgather, allgatherv

--- src/mpi/coll/coll_len_check.c
+++ src/mpi/coll/coll_len_check.c
@@ -61,6 +61,7 @@ int MPIR_Coll_len_check_scatter_1(MPI_Aint count, MPI_Datatype datatype, int roo
 
     sig = MPIR_Datatype_Init_Hash_Generator(datatype);
     sig.hash_value += root;
+    sig.num_types = count;
 
     mpi_errno = MPIR_Allreduce_equal(&sig, 2, MPI_UNSIGNED_LONG, &is_equal, comm_ptr, errflag);
 
@@ -82,10 +83,10 @@ int MPIR_Coll_len_check_scatter_2(MPI_Aint count, MPI_Datatype datatype, int roo
     int mpi_errno = MPI_SUCCESS;
 
     Type_Sig sig, rsig;
-    int is_equal;
 
     sig = MPIR_Datatype_Init_Hash_Generator(datatype);
     sig.hash_value += root;
+    sig.num_types = count;
 
     if(root == comm_ptr->rank){
         rsig.hash_value = sig.hash_value;
@@ -110,4 +111,190 @@ int MPIR_Coll_len_check_scatter_2(MPI_Aint count, MPI_Datatype datatype, int roo
             return mpi_errno;
         }  
     }
+}
+
+int MPIR_Coll_len_check_scatterv(MPI_Aint sendcount, MPI_Datatype sendtype, MPI_Aint* recvcount, MPI_Datatype recvtype, 
+		int root, MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
+{
+    int mpi_errno = MPI_SUCCESS;
+    Type_Sig sig, rsig[comm_ptr->size], temp_sig;
+    int i, is_equal;
+
+    sig = MPIR_Datatype_Init_Hash_Generator(sendtype);
+    sig.hash_value += root;
+    sig.num_types = sendcount;
+
+    mpi_errno = MPIR_Gather_impl(&sig, 2, MPI_UNSIGNED_LONG, rsig, 2, MPI_UNSIGNED_LONG, root, comm, errflag);
+
+    if(root == comm_ptr->rank){
+    	is_equal = 1;
+
+    	for(i = 0; i < comm_ptr->size; ++i)
+    	{
+    		temp_sig = MPIR_Datatype_Init_Hash_Generator(recvtype);
+    		temp_sig.num_types = recvcount[i];
+
+    		if(temp_sig.hash_value != rsig[i].hash_value || temp_sig.num_types != rsig[i].num_types)
+    			is_equal = 0;
+    	}
+    }
+
+    mpi_errno = MPIR_Bcast_impl(&is_equal, 1, MPI_INT, root, comm_ptr, errflag);
+
+    if(is_equal && (*errflag == MPIR_ERR_NONE))
+        return mpi_errno;
+    else
+    {
+        *errflag = MPIR_ERR_OTHER;
+        MPIR_ERR_SET2(mpi_errno, MPI_ERR_OTHER,
+            "**collective_size_mismatch",
+            "**collective_size_mismatch %d %d", root, comm_ptr->rank);
+        return mpi_errno;
+    }
+}
+
+int MPIR_Coll_len_check_allgather(MPI_Aint sendcount, MPI_Datatype sendtype, MPI_Aint recvcount, MPI_Datatype recvtype, 
+		MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
+{
+	int mpi_errno = MPI_SUCCESS;
+    Type_Sig sendsig, recvsig;
+    int i, is_equal;
+    unsigned long sendbuf[4];
+
+    sendsig = MPIR_Datatype_Init_Hash_Generator(sendtype);
+    sendsig.num_types = sendcount;
+
+    recvsig = MPIR_Datatype_Init_Hash_Generator(recvtype);
+    recvsig.num_types = recvcount;
+
+    sendbuf[0] = sendsig.hash_value;
+    sendbuf[1] = sendsig.num_types;
+    sendbuf[2] = recvsig.hash_value;
+    sendbuf[3] = recvsig.num_types;
+
+    mpi_errno = MPIR_Allreduce_equal(sendbuf, 4, MPI_UNSIGNED_LONG, &is_equal, comm_ptr, errflag);
+
+    if(is_equal && (*errflag == MPIR_ERR_NONE))
+    {
+    	/*
+    	Please verify: do recvcount = sendcount * numprocs needed in alltoall and allgather?
+    	 */
+    	if(sendsig.hash_value == recvsig.hash_value || 
+    			(sendsig.num_types * comm_ptr->size) == recvsig.num_types)
+    	{
+    		*errflag = MPIR_ERR_OTHER;
+	        MPIR_ERR_SET2(mpi_errno, MPI_ERR_OTHER,
+	            "**collective_size_mismatch",
+	            "**collective_size_mismatch %d", comm_ptr->rank);
+	        return mpi_errno;
+    	}
+
+        return mpi_errno;
+    }
+    else
+    {
+        *errflag = MPIR_ERR_OTHER;
+        MPIR_ERR_SET2(mpi_errno, MPI_ERR_OTHER,
+            "**collective_size_mismatch",
+            "**collective_size_mismatch %d", comm_ptr->rank);
+        return mpi_errno;
+    }
+}
+
+int MPIR_Coll_len_check_allgatherv(MPI_Aint sendcount, MPI_Datatype sendtype, MPI_Aint *recvcount, MPI_Datatype recvtype, 
+		MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
+{
+	struct allgatherv_info{
+		MPI_Aint recv_count[comm_ptr->size];
+		unsigned long send_hash_value;  
+        unsigned long recv_hash_value; 
+        unsigned long recv_num_types; 
+        int is_send_count_equal;
+        int is_equal;
+    }sendbuf, recvbuf;
+
+    MPI_Datatype derived_type;
+
+    int s_count = 6, s_blocklengths[6] = {comm_ptr->size, 1, 1, 1, 1, 1};
+    MPI_Aint s_displacements[6] = {offsetof(allgatherv_info, recv_count), offsetof(allgatherv_info, send_hash_value), 
+        	offsetof(allgatherv_info, recv_hash_value), offsetof(allgatherv_info, recv_num_types), 
+        	offsetof(allgatherv_info, is_send_count_equal), offsetof(allgatherv_info, is_equal)};
+    MPI_Datatype s_types[6] = {MPI_AINT, MPI_UNSIGNED_LONG, MPI_UNSIGNED_LONG, MPI_UNSIGNED_LONG, MPI_INT, MPI_INT};
+
+    int mpi_errno = MPI_SUCCESS;
+    Type_Sig sendsig, recvsig;
+    int i;
+
+    /* set up type */
+    mpi_errno = MPI_Type_create_struct(s_count, s_blocklengths, s_displacements, s_types, &derived_type);
+    MPI_Type_commit(&derived_type);
+
+    /* construct the struct to compare */
+    MPIR_Localcopy(sendbuf.recv_count, comm_ptr->size, MPI_Aint, recvcount, comm_ptr->size, MPI_Aint);
+
+	sendsig = MPIR_Datatype_Init_Hash_Generator(sendtype);
+    sendsig.num_types = sendcount;
+    recvsig = MPIR_Datatype_Init_Hash_Generator(sendtype);
+    recvsig.num_types = 0;
+
+    for(i = 0; i < comm_ptr->size; ++i)
+    {
+    	recvsig.num_types += recvcount[i];
+    }
+
+    sendbuf.send_hash_value = sendsig.hash_value;
+    sendbuf.recv_hash_value = recvsig.hash_value;
+    sendbuf.recv_num_types = recvsig.num_types;
+
+    if(sendsig.hash_value == recvsig.hash_value &&
+    		sendsig.num_types == recvcount[comm_ptr->rank])
+    	sendbuf.is_send_count_equal = 1;
+    else
+    	sendbuf.is_send_count_equal = 0;
+
+    sendbuf.is_equal = 1;
+
+    mpi_errno = MPIR_Allreduce_impl(&sendbuf, &recvbuf, 1, derived_type, MPIX_EQUAL, comm_ptr, errflag);
+
+    MPI_Type_free(&derived_type);
+
+    if(recvbuf.is_equal && (*errflag == MPIR_ERR_NONE))
+    {
+    	/*
+    	Please verify: do recvcount = sendcount * numprocs needed in alltoall and allgather?
+    	 */
+    	if(!sendbuf.is_send_count_equal)
+    	{
+    		*errflag = MPIR_ERR_OTHER;
+	        MPIR_ERR_SET2(mpi_errno, MPI_ERR_OTHER,
+	            "**collective_size_mismatch",
+	            "**collective_size_mismatch %d", comm_ptr->rank);
+	        return mpi_errno;
+    	}
+
+        return mpi_errno;
+    }
+    else
+    {
+        *errflag = MPIR_ERR_OTHER;
+        MPIR_ERR_SET2(mpi_errno, MPI_ERR_OTHER,
+            "**collective_size_mismatch",
+            "**collective_size_mismatch %d", comm_ptr->rank);
+        return mpi_errno;
+    }
+}
+
+
+int MPIR_Coll_len_check_alltoallv(MPI_Aint sendcount, MPI_Datatype sendtype, MPI_Aint *recvcount, MPI_Datatype recvtype, 
+		MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
+{
+
+	return mpi_errno;
+}
+
+int MPIR_Coll_len_check_alltoallw(MPI_Aint sendcount, MPI_Datatype sendtype, MPI_Aint *recvcount, MPI_Datatype recvtype, 
+		MPIR_Comm * comm_ptr, MPIR_Errflag_t * errflag)
+{
+
+	return mpi_errno;
 }
